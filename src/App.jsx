8147b2b56f6012dbccff03b69cde12a1e7b8011a
Nikhil p <nikhilppry2001@gmail.com>
2024-11-30 06:38:52 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Aboutme from './Component1/Aboutme';
+import Skills from './Component2/Skills.jsx';
+import Project from './Component3/Project.jsx'
+import Contact from './Component4/Contact.jsx'
+const App = () => {
+  return (
+    <>
+      <Aboutme />
+      <Skills />
+      <Project />
+      <Contact />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Component2/Skills.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Container,Button } from 'react-bootstrap';
+import './Skills.css'
+const Skills = () => {
+  return (
+    <body className='index-1'>
+    <Container>
+<br /><br /><br />
+
+<h1 className='index-2'>Skills</h1>
+<br /><br />
+<main className='index-3'>
+<div>
+    <h2 className='index-4'>Front End<br />
+    <br />
+    HTML5<br />
+    CSS3,<br />
+Bootstrap5,<br />
+JavaScript,<br />
+TypeScript,<br />
+Angular,<br />
+React,
+    </h2>
+<br />
+</div>
+<div >
+    <h2 className='index-5'>Back end<br />
+    <br />Node js,<br />
+Mongodb,
+    </h2>
+</div>
+<div>
+
+<h2 className='index-30'>Version Controler<br /><br />Git,<br />
+Github,<br />
+SourceTree</h2>
+</div>  
+</main>
+    </Container>
+    </body>
+  )
+}
+
+export default Skills
